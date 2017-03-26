7661d75278e7247df15785e88c1399d3a90d64ef
robertnewey <robert@nuiz.org>
2017-03-27 02:10:32 +1100

    Update settings for new domain

--- apio2017-web/settings.py
+++ apio2017-web/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = MY_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'apio2016.org', 'www.apio2016.org']
+ALLOWED_HOSTS = ['127.0.0.1', 'apio17.org', 'www.apio17.org']
 
 
 # Application definition
